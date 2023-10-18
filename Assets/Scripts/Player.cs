eaf0c6d0763881e318da4736a028fa1e804b6752
ShubaShaba <shub21680@gmail.com>
2023-10-18 22:23:37 +0200

    Test different approaches to processing player's physics

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,14 +12,34 @@ public class Player : MonoBehaviour {
 
     // Update is called once per frame
     private void Update() {
+        this.MovementHandler();
+        this.InteractionHandler();
+    }
+
+    private void InteractionHandler () {
+        float interactionDistance = 2f;
+
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit interactionObject, interactionDistance)) {
+            Debug.Log(interactionObject.transform);
+        }
+    }
+    /*
+    TODO: 
+    Rigidbody physics is processed within FixedUpdate() => Mixing it with transform.postion modification from Update()
+    causes visuals jitters in player's movement. 
+
+    Solution 1: Handle movement in FixedUpdate(). (Not a scalable solution in terms of the frames per second)
+    Solution 2: Write a simple gravitational script. 
+     */
+    private void MovementHandler() {
         float playerHeight = 2f;
         float playerRadius = .7f;
         float moveDistance = moveSpeed * Time.deltaTime;
 
         // Calculating the direction of movement relative to the camera
         Vector3 inputDirection = input.getInputDirectionNormalized();
-        Vector3 camForward = new Vector3 (cameraPosition.forward.x, 0, cameraPosition.forward.z);
-        Vector3 camRight = new Vector3 (cameraPosition.right.x, 0, cameraPosition.right.z);
+        Vector3 camForward = new Vector3(cameraPosition.forward.x, 0, cameraPosition.forward.z);
+        Vector3 camRight = new Vector3(cameraPosition.right.x, 0, cameraPosition.right.z);
         Vector3 moveDirection = (inputDirection.z * camForward + inputDirection.x * camRight).normalized;
 
         float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
