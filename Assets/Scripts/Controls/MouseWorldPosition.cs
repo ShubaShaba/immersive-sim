87a57413528d6a7eb585888309241228c006111d
ShubaShaba <shub21680@gmail.com>
2023-12-02 17:29:45 +0100

    Refactor file structure (Logic-UI-Controls)

--- Assets/Scripts/Controls/MouseWorldPosition.cs
+++ Assets/Scripts/Controls/MouseWorldPosition.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class MouseWorldPosition : MonoBehaviour {
-    [SerializeField] private PlayerInput input;
     [SerializeField] private Camera aimingCamera;
 
     private void Update () {
